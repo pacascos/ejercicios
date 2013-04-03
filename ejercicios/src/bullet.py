07a1fd1d5caddb83c7e8fb8e8cdc115b662c821e
cascos <cascos@192.168.1.30>
2013-04-04 00:01:24 +0200

    prueba fallida, comprobar que el muro esta antes del vector del disparo

--- ejercicios/src/bullet.py
+++ ejercicios/src/bullet.py
@@ -28,29 +28,48 @@ class linea:
 def checkio(data):
     xw1, yw1 = data[0]
     xw2, yw2 = data[1]
+
     xa, ya = data[2]
     xb, yb = data[3]
     
     meta = linea(punto(xw1,yw1),punto(xw2,yw2))
     bala = linea(punto(xa,ya),punto(xb,yb))
-    resultado = meta.intersecta(bala)
+    try:
+        resultado = meta.intersecta(bala)
+    except:
+        print('division 0')
+        return(xa/ya == xb/yb)
     print ('------',resultado)
-    print ('x: ',xw1,'<=',resultado.x,'<=',xw2,'-->',(xw1 <= resultado.x and resultado.x <= xw2))
-    print ('y: ',yw1,'<=',resultado.y,'<=',yw2,'-->',(yw1 <= resultado.y  and resultado.y <= yw2))
-    return ((xw1 <= resultado.x and resultado.x <= xw2) and (yw1 <= resultado.y  and resultado.y <= yw2))
+    print ('x: ',min(xw1,xw2),'<=',resultado.x,'<=',max(xw1,xw2),'-->',(min(xw1,xw2) <= resultado.x and resultado.x <= max(xw1,xw2)))
+    print ('y: ',min(yw1,yw2),'<=',resultado.y,'<=',max(yw1,yw2),'-->',(min(yw1,yw2) <= resultado.y  and resultado.y <= max(yw1,yw2)))
+    
+
+    if xa<xb:
+        corta = resultado.x >= xa
+    else:
+        corta = resultado.x <= xb
+    if ya<yb:
+        corta2 = resultado.y >= ya
+    else:
+        corta2 = resultado.y <= yb
+    print('corta ',corta)
+    print('corta2 ',corta2)
+    return ((min(xw1,xw2) <= resultado.x and resultado.x <= max(xw1,xw2)) and (min(yw1,yw2) <= resultado.y  and resultado.y <= max(yw1,yw2)) and corta and corta2)
 
     
-    
-    return True
+
     
 if __name__=="__main__":
-    l1 =linea(punto(0,0),punto(2,2))
-    l2 =linea(punto(6,0),punto(3,1))  
+    #l1 =linea(punto(0,0),punto(2,2))
+    #l2 =linea(punto(6,0),punto(3,1))  
     #print (l1,l2)
     #print (l1.intersecta(l2))
+    '''
     assert checkio([[0,0], [0,2], [5,1], [3,1]]) == True, "First"
     assert checkio([[0, 0], [0, 2], [3, 1], [5, 1]]) == False, "Reverse First"
     assert checkio([[0, 0], [2, 2], [6, 0], [3, 1]]) == True, "Second"
     assert checkio([[6, 0], [5, 5], [4, 0], [5, 6]]) == False, "Third"
     assert checkio([[0, 0], [1, 1], [3, 3], [2, 2]]) == True, "Fourth, shot in butt of wall :)"
     assert checkio([[0, 0], [1, 1], [3, 2], [2, 1]]) == False, "Fifth, parallel"
+    '''
+    assert checkio([[5,4], [9,9], [8,4], [0,6]]) == True
